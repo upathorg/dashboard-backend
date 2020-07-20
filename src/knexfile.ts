6bf4221c08ac4b886cfbab40ed63b5b1749972b5
chidibede <chidibede@gmail.com>
2020-07-21 00:41:32 +0100

    Heroku's initial deployment setup

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -47,17 +47,12 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
 
     migrations: {
       tableName: "knex_migrations",
-      directory: "./data/migrations",
+      directory: "./migrations",
     },
 
     seeds: {
--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,16 @@ import { typeDefs } from "./graphql/schema";
 const app: Application = express();
 
 // Set up Apollo Server
-const apolloServer: ApolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer: ApolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 apolloServer.applyMiddleware({ app, path: "/graphql" });
-
 
 app.use(cors());
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on localhost:${port}/graphql`);
 });
-
-
